78e215168422698305ad09493aa9e54128401ed7
Bodzey <djsteward.rv@gmail.com>
2021-08-29 15:25:10 +0300

    update js
    random circle color

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+const colors = ['#f1ff75', '#9fea15', '#ee75f4', '#0a7f27', '#e5097e', '#5fa4dd', '#93ff97', '#998cff', '#d6a1fc', 'aefce8', '#70ef72', '#92fce3', '#f9b28e', '#b481e8', '#f9c5a4', '#95fc76', '#d39ef7', '#d9f274', '#b0f49a', '#aecafc', '#958af7', '#7b93f2', '#8df4a0', '#a4fcd0', '#b8cafc', '#ea72f9', '#f0fc94', '#97f4c4', '#9cfcee', '#ea72ae', '#97c1ef', '#a1c8ed', '#99f984', '#b8f49a', '#b5cff4', '#ae71e8', '#f2cf85', '#fcd0a4', '#ffea9e', '#f7a088', '#e8fc99', '#9e6ed8', '#ffd9cc', '#d186f4', '#f280a2', '#68c5d8', '#96ffa0', '#e0a4f9', '#fcb5ca', '#e3a9f9', '#a37de0', '#91ffff', '#d5ef88', '#98ea6b', '#f9e595', '#f7e5af', '#ed93de']
+
 
 let time = 0
 let score = 0
@@ -62,16 +64,23 @@ function createRandomCircle() {
     const {width, height} = board.getBoundingClientRect()
     const x = getRandomNumber(0, width - size)
     const y = getRandomNumber(0, height - size)
+    const color = getRandomColor()
 
     circle.classList.add('circle')
     circle.style.width = `${size}px`
     circle.style.height = `${size}px`
     circle.style.top = `${y}px`
     circle.style.left = `${x}px`
+    circle.style.background = color
 
     board.append(circle)
 }
 
 function getRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+     
+    return colors[Math.floor(Math.random() * colors.length)]
 }
